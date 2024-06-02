ae30f6f01cb06920ca3c5a04d9bb0b709c2e3b12
Sakshi Choudhary <103510144+sakshi-1140@users.noreply.github.com>
2024-06-03 01:25:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
+	 const Form=document.getElementById("form1")
+    const inputs=Form.elements
+    const firstName=inputs["fname"].value
+    const lastName=inputs["lname"].value
+    alert(firstName+" "+lastName);
 
 }
